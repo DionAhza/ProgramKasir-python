53a9fed0e67cbfd3b0e77503a71838926bceb6af
DionAhza <115599030+DionAhza@users.noreply.github.com>
2024-10-16 11:56:10 +0700

    Update kasir.py

--- kasir.py
+++ kasir.py
@@ -3,63 +3,75 @@ barang = []
 harga = []
 
 while True: 
-    print("""Daftar Barang\n
-        1. Roti \t 5000
-        2. Es Krim \t 7000
-        3. Keripik \t 8000
-        4. Coklat \t 12000
-        5. Buku \t 10000
-        6. Pensil \t 4000
-        7. Penghapus \t 3000
-        """
-          )
-    
-    kode = int(input("masukan Kode Barang : "))
+    print("""
+Daftar Barang
+    1. Roti \t 5000
+    2. Es Krim \t 7000
+    3. Keripik \t 8000
+    4. Coklat \t 12000
+    5. Buku \t 10000
+    6. Pensil \t 4000
+    7. Penghapus \t 3000
+    """)
+
+    try:
+        kode = int(input("Masukkan Kode Barang : "))
+    except ValueError:
+        print("Kode harus berupa angka!\n")
+        continue
+
     if kode == 1:
-        barang.append('roti')
+        barang.append('Roti')
         harga.append(5000)
         total += 5000
     elif kode == 2:
-        barang.append('es krim')
-        barang.append(7000)
+        barang.append('Es Krim')
+        harga.append(7000)
         total += 7000
     elif kode == 3:
-        barang.append('keripik')
+        barang.append('Keripik')
         harga.append(8000)
         total += 8000
     elif kode == 4:
-        barang.append('coklat')
+        barang.append('Coklat')
         harga.append(12000)
         total += 12000
     elif kode == 5:
-        barang.append('buku')
+        barang.append('Buku')
         harga.append(10000)
         total += 10000
     elif kode == 6:
-        barang.append('pensil')
+        barang.append('Pensil')
         harga.append(4000)
         total += 4000
     elif kode == 7:
-        barang.append('penghapus')
+        barang.append('Penghapus')
         harga.append(3000)
         total += 3000
     else:
-        print('Kode tidak Valid')
+        print("Kode tidak valid!\n")
+        continue
 
-    lanjut =  input('lanjut belanja (y/n) : ')
+    lanjut = input('Lanjut belanja (y/n) : ').lower()
     if lanjut == 'n':
-            print("")
-            break
-    print('barang yang dibeli:', barang)
-    print('harga barang:', harga)
-    print('total harga yang harus dibayar',total,'\n')
+        print("\nDaftar Belanja:")
+        for i in range(len(barang)):
+            print(f"{i + 1}. {barang[i]} - {harga[i]}")
+        print(f"\nTotal harga yang harus dibayar: {total}\n")
+        break
 
-    uang = int(input('masukan uang pembayaran : '))
+while True:
+    try:
+        uang = int(input('Masukkan uang pembayaran: '))
+    except ValueError:
+        print("Masukkan jumlah uang yang valid!\n")
+        continue
+
     if uang > total:
-        print('kembaliannya : ',uang - total)
+        print(f"Kembaliannya: {uang - total}\nTerima kasih telah berbelanja!")
+        break
     elif uang == total:
-        print('uangnya pas')
+        print("Uangnya pas. Terima kasih telah berbelanja!")
+        break
     else:
-        print('uangnya kurang',uang - total)
-
-
+        print(f"Uangnya kurang {total - uang}. Silakan masukkan jumlah yang tepat.\n")
